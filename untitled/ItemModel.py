4581a00040125a0b66679c4252d9fcff0fa0a5bb
ThomOverhand <thom_overhand@hotmail.com>
2016-10-25 16:03:26 +0200

    Added the filter options

--- untitled/ItemModel.py
+++ untitled/ItemModel.py
@@ -3,26 +3,38 @@ from flask import jsonify
 
 
 class ItemModel():
-    def __init__(self, name, description, price):
+
+    itemlst = []
+
+    def __init__(self, name, description, price, classification):
         self.name = name
         self.description = description
         self.price = price
+        self.classification = classification
 
     @staticmethod
     def get_all_items():
-        with open('/home/hachiko/PycharmProjects/untitled/products.json') as json_data:
-            return json.load(json_data)
-
+        with open('/home/hachiko/Documents/Project5-6/untitled/products.json') as json_data:
+            shit = json.load(json_data)
+            ItemModel.itemlst = []
+            for i in shit:
+                ItemModel.itemlst.append(ItemModel(i["name"], i["description"], i["price"], i["class"]))
+            return shit
 
     @staticmethod
     def filter_item_name(name):
-        data = ItemModel.get_all_items()
-        output = [item for item in data if item['name'] == name]
-        return json.dumps(output)
+        output = [item for item in ItemModel.itemlst if item.name == name]
+        return json.loads(json.dumps([o.__dict__ for o in output]))
     
+    @staticmethod
+    def filter_item_price(min, max):
+        output = [item for item in ItemModel.itemlst if (min <= float(item.price) <= max) ]
+        return json.loads(json.dumps([o.__dict__ for o in output]))
 
-
-
+    @staticmethod
+    def filter_item_classifiction(classification):
+        output = [item for item in ItemModel.itemlst if item.classification == classification]
+        return json.loads(json.dumps([o.__dict__ for o in output]))
 """
 $.ajax({
     type: "GET",
--- untitled/untitled.py
+++ untitled/untitled.py
@@ -1,17 +1,23 @@
 from flask import Flask, request,jsonify
 from ItemModel import *
 
-
 app = Flask(__name__)
-
 
 @app.route('/items')
 
-def items():
+def items(name = None, classification=None):
     name = request.args.get("name")
-    if(name):
-        return ItemModel.filter_item_name(str(name))
-    return jsonify(ItemModel.get_all_items())
+    min = request.args.get("min")
+    max = request.args.get("max")
+    classification = request.args.get("class")
+    items = ItemModel.get_all_items()
+    if(name != None):
+        return jsonify(ItemModel.filter_item_name(str(name)))
+    if(classification != None):
+        return jsonify(ItemModel.filter_item_classifiction(str(classification)))
+    if(min and max):
+        return jsonify(ItemModel.filter_item_price(float(min),float(max)))
+    return jsonify(items)
 
 if __name__ == '__main__':
     app.run(debug=True)
