c5ea72b437fc84b0215983718c2c94c163162acd
FireStormHR <bret_hart_hitman_raw@hotmail.com>
2016-10-26 10:36:45 +0200

    database so far

---
+++ Database/creating the tables.sql
@@ -0,0 +1,31 @@
+﻿drop table if exists User_;
+drop table if exists Buyable_item_;
+
+create table User_ (
+User_ID SERIAL primary key,
+Credit float,
+Street char(50),
+Street_number char(6),
+Postal_code char(6),
+First_name char(20),
+Surname char(35),
+Adminbool boolean,
+Birthday date,
+Wachtwoord char(20),
+Email_address char(45)
+);
+
+create table Buyable_item_(
+Product_ID SERIAL primary key,
+Title char(50),
+Description char(700),
+In_stock int,
+Fits_in_mailbox boolean,
+price float);
+
+
+
+
+
+
+--#timestamp [ (p) ] [ without time zone ]
