a6f11390b6121466554608774050befd8884f756
ThomOverhand <Thom_overhand@hotmail.com>
2016-10-26 17:17:20 +0200

    Added handlebar

--- untitled/static/test.js
+++ untitled/static/test.js
@@ -1,13 +1,31 @@
 $(document).ready(function(){
 
-    $("p").click(
-        function(){
-            console.log("shit");
-        }
-    );
+    var source   = $("#entry-template").html();
+    var template = Handlebars.compile(source);
+    var context = {title: "My New Post", body: "This is my first post!"};
+    var html    = template(context);
+    console.log(html);
+    $('.test').append(html);
 
-    $(".inner").append(
-        "<p> test </p>"
-    )
+    $("#shit").click(function(param){
+        $.ajax({       
+            url: "http://localhost:5000/" + param,   
+        }).done(function(json){
+                
+        }); 
+
+    });
+    
+    $.ajax({       
+        url: "http://localhost:5000/static/ProductPanel.html"
+ 
+        }).done(function(json){
+
+                    var template = Handlebars.compile(json);
+                    var context = {title: "My New Post", body: "This is my first post!"};
+                    var html    = template(context);
+                    console.log(html);
+                    $('.test').append(html);
+        }); 
 
 });
