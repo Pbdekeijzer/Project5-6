359d2bac32ece6cd2c7b31ec88c996a31bd0a34c
Thom Overhand <thom_overhand@hotmail.com>
2016-10-26 21:49:16 +0200

    Added option to filter

--- untitled/static/test.js
+++ untitled/static/test.js
@@ -1,31 +1,63 @@
 $(document).ready(function(){
 
-    var source   = $("#entry-template").html();
-    var template = Handlebars.compile(source);
-    var context = {title: "My New Post", body: "This is my first post!"};
-    var html    = template(context);
-    console.log(html);
-    $('.test').append(html);
+    // Inserts HTML into the product template and appends the HTML in the index.
+    // param = json
+    function InsertProduct(json){
+        $.ajax({       
+            url: "http://localhost:5000/static/ProductPanel.html"
+        }).done(function(data){
+            var container = $("#test");
+            var row = $("<div class='row'></div>");
+            container.append(row);
+            for(var i = 0; i < json.length; i++){
+                if(i % 3 == 0){
+                    row = row = $("<div class='row'></div>");
+                    container.append(row)
+                }
+                var template = Handlebars.compile(data);
+                var context = {title: json[i].name, body: json[i].description};
+                var html    = template(context);
+                row.append(html);
+            }
+        });
+    };
 
-    $("#shit").click(function(param){
+    // GET JSON from URL, call insert product.
+    // param = string
+    function GetJSONFromUrl(param){
         $.ajax({       
             url: "http://localhost:5000/" + param,   
         }).done(function(json){
-                
-        }); 
+             InsertProduct(json);
+        });     
+    };
 
-    });
-    
-    $.ajax({       
-        url: "http://localhost:5000/static/ProductPanel.html"
- 
-        }).done(function(json){
+    function ReadyItemArguments(name, properties, minprice, maxprice){
+        var address = "items?"
+        if(name != null){
+            address += "name=" + name;
+        }
 
-                    var template = Handlebars.compile(json);
-                    var context = {title: "My New Post", body: "This is my first post!"};
-                    var html    = template(context);
-                    console.log(html);
-                    $('.test').append(html);
-        }); 
+        if(properties != null){
+            if( typeof properties === 'string' ) {
+                properties = [ properties ];
+            }
+            address += "&property="
+            properties.forEach(function(value, index, array){
+                address+= value;
+                if(index != (array.length-1)){
+                    address+= "+";
+                }
+            });
+        }
 
+        if(maxprice != null){
+            address+= "&maxprice=" + maxprice;
+        }
+        if(minprice != null){
+            address+= "&minprice=" + minprice;
+        }
+        GetJSONFromUrl(address);
+    }
+    ReadyItemArguments(null, null, null, null);
 });
