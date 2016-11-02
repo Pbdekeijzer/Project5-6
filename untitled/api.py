e0d26a76b14a827e03cd8eb46d98fd2847c3af99
ThomOverhand <Thom_overhand@hotmail.com>
2016-11-02 12:18:30 +0100

    Small api change

--- untitled/api.py
+++ untitled/api.py
@@ -28,6 +28,17 @@ def register():
         return "yay" 
         
     return render_template('register.html')
+# @app.route('/login', methods = ['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         username = request.form['username']
+#         password = request.form['password']
+#         # user = db.find_user(username, password)
+#         # if not user -> raise error
+#         response = redirect(url_for("userpage"))
+#         response.set_cookie("shit", user.id)
+#         return response
+
 @app.route('/accounts')
 
 def accounts():
