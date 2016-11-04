594e8a36afc5baaf3ff7f868154a9855665b2e99
FireStormHR <bret_hart_hitman_raw@hotmail.com>
2016-11-04 10:55:21 +0100

    wishlist button works now

--- untitled/static/Javascripts/products.js
+++ untitled/static/Javascripts/products.js
@@ -52,59 +52,12 @@ $(document).ready(function(){
     });
 
 
-var IsInWishlist;
-    $("#wishlistButton").click(function(){
-
-    setTimeout(function(){
-        //TODO: Check if user id logged in, hide button if not
-        //TODO: Selectquery check if product is in users wishlist, change var accordingly
-        IsInWishlist = true; //temp, remove if todos or done
-        if (IsInWishlist == true){
-            $('#wishlistButton').text("Remove from wishlist");
-        }
-        else if (IsInWishlist == false){
-            $('#wishlistButton').text("Add to wishlist");
-        }
-    }, 1000);
-
-
-
-
-    $('#wishlistButton').click(function(){
-        if (IsInWishlist == true){
-            //TODO: Delete query, delete product from wishlist
-            IsInWishlist = false;
-            $('#wishlistButton').text("Add to wishlist");
-            $('#Notification').text('Succesfully removed from wishlist');
-        }
-        else if (IsInWishlist == false){
-            //TODO: Insert query, delete product from wishlist
-            IsInWishlist = true;
-            $('#wishlistButton').text("Remove from wishlist");
-            $('#Notification').text('Succesfully added from wishlist');
-
-
-        }
-        
-
-
-    });
-
-
-
-
-
 
 
     // Does something after 0.5 second, sets string for div with id=amountinstock
     setTimeout( function(){ 
         $('#AmountInStock').text('Amount in stock: '+ in_stock); 
     }  , 1000 );
-
-    //On click is going to do something
-    $("#AddToWishlist").click(function(){
-        //Do something, yet to come
-    });
 
 
 });
