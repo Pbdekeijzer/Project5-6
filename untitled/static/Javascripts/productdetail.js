dd12ca6013a7cad26db553255b85615b200abdab
FireStormHR <bret_hart_hitman_raw@hotmail.com>
2016-11-07 12:34:16 +0100

    A lot of extra functionalities on the wishlist button

--- untitled/static/Javascripts/productdetail.js
+++ untitled/static/Javascripts/productdetail.js
@@ -2,10 +2,19 @@ $(document).ready(function(){
 
     var pathname = $(location).attr('pathname');
     var suburl = pathname.substring(pathname.lastIndexOf('/') + 1);
-    var inWishlist = false;
+    var inWishlist = false; //remove the false when done
+    var jsonjs;
+    GetItemJson();
+    //CheckIfProductInWishlist();
+
 
     $('#wishlistButton').click(function(){
-        GetItemJson();
+        if (inWishlist == true){
+            RemoveFromWishlist(jsonjs);
+        }
+        else if (inWishlist == false){
+            AddToWishlist(jsonjs);
+        }
     });
 
     function GetItemJson(){
@@ -13,14 +22,35 @@ $(document).ready(function(){
             url: "http://localhost:5000/items?id=" + suburl
         }).done(function(json){
             json = JSON.stringify(json[0]);
-            if(inWishlist){
-                RemoveFromWishlist(json);
-            }
-            else{
-                AddToWishlist(json);
-            }
+            var context = {title: json.name, body: json.description, image: json.image, id: json.id, continent: json.continent, classification: json.class, price: json.price};
+            jsonjs = json;
         });
     };
+
+    // function CheckIfProductInWishlist(){   
+    //     $.ajax({
+    //         url: "http://localhost:5000/account/wishlist",
+    //     }).done(function(json){
+    //         json = JSON.stringify(json[0]);
+    //         inWishlist = false;
+            
+    //         for(var i in json){
+	// 			var context = {title: json[i].name, body: json[i].description, image: json[i].image, id: json[i].id, continent: json[i].continent, classification: json[i].class, price: json[i].price};
+	// 			if (context == jsonjs){
+    //                 inWishlist = true;
+    //                 console.log("I found it!!!");
+    //             }
+    //         }
+
+    //         if(inWishlist == true){
+    //             $('#wishlistButton').text("Remove from wishlist");
+    //         }
+    //         else{
+    //             $('#wishlistButton').text("Add to wishlist");
+    //         }
+    //     });
+    // };
+
 
     function RemoveFromWishlist(json){
         $.ajax({
@@ -30,6 +60,9 @@ $(document).ready(function(){
             contentType: "application/json"
         }).done(function(){
             inWishlist = false;
+            $('#wishlistButton').text("Add to wishlist");
+            $('#Notification').text('Succesfully removed from wishlist');
+
         });
     };
 
@@ -41,6 +74,9 @@ $(document).ready(function(){
             contentType: "application/json"
         }).done(function(){
             inWishlist = true;
+            $('#wishlistButton').text("Remove from wishlist");
+            $('#Notification').text('Succesfully added to wishlist');
+
         });
     };
 });
