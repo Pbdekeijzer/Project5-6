3217799bd6b1250704aefcf5c932ad482f8bf9ad
Pbdekeijzer <Pbdekeijzer@hotmail.com>
2016-11-07 14:26:33 +0100

    some more fixes

--- untitled/static/Javascripts/productdetail.js
+++ untitled/static/Javascripts/productdetail.js
@@ -3,11 +3,13 @@ $(document).ready(function(){
     var pathname = $(location).attr('pathname');
     var suburl = pathname.substring(pathname.lastIndexOf('/') + 1);
     var inWishlist = false; //remove the false when done
-    GetItemJson();
-    //CheckIfProductInWishlist();
-    var inWishlist = false;
+    var jsonjs;
     var wishlistitems = [];
     getWishlistIDs();
+    GetItemJson();
+    //CheckIfProductInWishlist();
+
+
 
 
     if(window.document.cookie){
@@ -34,10 +36,7 @@ $(document).ready(function(){
                     console.log(element);
                     if (element == suburl){
                         $("#wishlistButton").text("Remove from wishlist");
-                        inWishlist == true;
-                    }
-                    else{
-                        console.log('FALSESESSEAS');
+                        inWishlist = true;
                     }
                 });        
             }); 
@@ -52,52 +51,15 @@ $(document).ready(function(){
         }
     });
 
-    // function GetItemID(){
-    //     $.ajax({
-    //         url: "http://localhost:5000/items?id=" + suburl
-    //     }).done(function(json){
-    //         json = JSON.stringify(json); 
-    //         $.each(JSON.parse(json), function(idx, obj) {
-	//             itemid.push(obj.id); 
-    //         });
-
-    //         console.log(itemid[0])            
-    //     });
-    // };
-
 
     function GetItemJson(){
         $.ajax({
             url: "http://localhost:5000/items?id=" + suburl
         }).done(function(json){
             json = JSON.stringify(json[0]);
+            jsonjs = json;
         });
     };
-
-    // function CheckIfProductInWishlist(){   
-    //     $.ajax({
-    //         url: "http://localhost:5000/account/wishlist",
-    //     }).done(function(json){
-    //         json = JSON.stringify(json[0]);
-    //         inWishlist = false;
-            
-    //         for(var i in json){
-	// 			var context = {title: json[i].name, body: json[i].description, image: json[i].image, id: json[i].id, continent: json[i].continent, classification: json[i].class, price: json[i].price};
-	// 			if (context == jsonjs){
-    //                 inWishlist = true;
-    //                 console.log("I found it!!!");
-    //             }
-    //         }
-
-    //         if(inWishlist == true){
-    //             $('#wishlistButton').text("Remove from wishlist");
-    //         }
-    //         else{
-    //             $('#wishlistButton').text("Add to wishlist");
-    //         }
-    //     });
-    // };
-
 
     function RemoveFromWishlist(json){
         $.ajax({
