1aac25bb938e829b53710f3c2da7529e8c43a9a6
FireStormHR <bret_hart_hitman_raw@hotmail.com>
2016-12-03 13:04:10 +0100

    Cookie is now read, and can be used all for the pages

--- untitled/static/Javascripts/mainpage.js
+++ untitled/static/Javascripts/mainpage.js
@@ -225,9 +225,18 @@ $(document).ready(function(){
 		
 		//reads cookie and shows if user is logged in or not
 		if (window.document.cookie){
-			var Howdoesmycookielook = window.document.cookie.toString(); //you can put a breakpoint in the browser at this line, so you can check what the cookie contains
-			var username = window.document.cookie.toString().split('=');
-			$("#UserLoggedInNotification").text("Logged in as: " + username[1]);
+			var Howdoesmycookielook = window.document.cookie.toString(); 			//you can put a breakpoint in the browser at this line, so you can check what the cookie contains
+			var Thecookiedivided = window.document.cookie.toString().split('=');	//Puts the data in an array, data is divided by the = mark
+			username = Thecookiedivided[1].slice(1); 								//Slice because first element is a "
+			$("#UserLoggedInNotification").text("Logged in as: " + username);
+			adminbool = parseInt(Thecookiedivided[2]);								//The adminbool is an integer, with 0 or 1 as the bool value
+			
+			if (adminbool == 1){ 													//notify user he is an admin
+				$("#UserLoggedInNotification").text(  $("#UserLoggedInNotification").text()+"              You are an admin!"  );
+				//TODO: append an admin link in the navbar
+			}
+
+
 			$('#NavbarAtTop').append('<li><a href= "http://localhost:5000/logout" id="LogoutNavbar">Log Out</a></li>');
 		}
 		else{
@@ -235,6 +244,8 @@ $(document).ready(function(){
 			$('#NavbarAtTop').append('<li><a href="/login" id="LoginNavbar">Login</a></li>');
     	    $('#NavbarAtTop').append('<li><a id="registershit" href="/register">Register</a></li>');			
 		}
+
+
 
 
 	});
