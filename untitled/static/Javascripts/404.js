6963edc080882b49df1c165fe6cd359e45917a5d
SG1995 <0898958@hr.nl>
2017-01-16 00:45:56 +0100

    Tried sound and failed :(

---
+++ untitled/static/Javascripts/404.js
@@ -0,0 +1,21 @@
+//var audioElement = document.createElement('audio');
+//audioElement.setAttribute('src', 'http://localhost:5000/static/images/pandaaudio.mp3');
+//
+////$.get();
+//
+//audioElement.addEventListener("load", function() {
+//	audioElement.play();
+//}, true);
+//$(document).ready(function() {
+//    var audioElement = document.createElement('audio');
+//    audioElement.setAttribute('src', 'http://localhost:5000/static/images/pandaaudio.mp3');
+//
+//
+//    audioElement.addEventListener('load', function() {
+//        this.play();
+//    }, false);
+//
+//    audioElement.addEventListener('load', function() {
+//        $("#audio")[0].play();
+//    }, false);
+//});
