2d2017cee1ae6cc127f69209ba16a9564df86841
Pbdekeijzer <Pbdekeijzer@hotmail.com>
2017-01-16 11:53:16 +0100

    huehue

--- untitled/static/Javascripts/wishlist.js
+++ untitled/static/Javascripts/wishlist.js
@@ -31,40 +31,8 @@ $(document).ready(function(){
             RemoveHTMLPanels();
             InsertProduct(json);
         });     
-    };
-
-    if (window.document.cookie){
-        var username = window.document.cookie.toString().split('=');
-        account_url = "http://localhost:5000/account/"  + username[1];
-        $('#NavbarAtTop').append('<li><a href= ' + account_url +  ' id="Account">' + username[1] + '</a></li>');
-        $('#NavbarAtTop').append('<li><a href= "http://localhost:5000/logout" id="LogoutNavbar">Log Out</a></li>');
-        $('#LogoutNavbar').click(function(){
-            localStorage.clear();
-        });
-    }
-    else{  
-        var timer = 4;     
-        $('#NavbarAtTop').append('<li><a href="/login" id="LoginNavbar">Login</a></li>');
-        $('#NavbarAtTop').append('<li><a id="registershit" href="/register">Register</a></li>');   
-        $('#wishlist-redirect').text('You must be logged in to see the wishlist.'); 
-        $('#wishlist-redirection').text('Redirecting in ')
-        $('#redirect-timer').text(timer);
-
-
-        setInterval(function(){
-            timer -= 1;
-            $('#redirect-timer').text(timer);
-
-            if (timer == 0){
-                window.location.href = "/login";
-            }
-        }, 1000); 
-       
-     
+    }; 
         // setTimeout(function(){
         //     window.location.href = "/login";
         // }, 4000);          
-    }
-
-
 });
