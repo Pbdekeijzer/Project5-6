e9b7c214ee3ff0a096740a718e10af92970f71ef
ThomOverhand <Thom_overhand@hotmail.com>
2017-01-24 13:43:28 +0100

    added unit tests for the accountmodel

--- app/tests/test_models.py
+++ app/tests/test_models.py
@@ -4,7 +4,9 @@ from app.models.AccountModel import *
 
 class TestAccount(unittest.TestCase):
     def setUp(self):
-        self.account = AccountModel(uid = 100, username = "John", password = "Snow", email = "john_snow@email.com", postal_code="1111QQ", house_number=1)
+        self.account = AccountModel(username = "John", password = "Snow", email = "john_snow@email.com", postal_code="1111QQ", house_number=1)
+        self.account.insertAccount()
+        self.uid = AccountModel.getUID(self.account.username)
 
     def test_init(self):
         self.assertEqual(self.account.username, "John")
@@ -12,5 +14,34 @@ class TestAccount(unittest.TestCase):
         self.assertEqual(self.account.email, "john_snow@email.com")
         self.assertEqual(self.account.postal_code, "1111QQ")
         self.assertEqual(self.account.house_number, 1)
+        self.assertEqual(self.account.adminbool, 0)
+        self.assertEqual(self.account.privacywishlist, 1)
+        self.assertEqual(self.account.blockedbool, 0)
+
+    def test_toDict(self):
+        dictionary = {
+            "uid": 0,
+            "username": "John",
+            "password": "Snow",
+            "email": "john_snow@email.com",
+            "postal_code": "1111QQ",
+            "house_number": 1,
+            "adminbool": 0,
+            "privacywishlist": 1,
+            "blockedbool": 0 
+        }
+        self.assertEqual(self.account.toDict(), dictionary)
+
+    def test_getUID(self):
+        uid = AccountModel.getUID(self.account.username)
+        self.assertEqual(type(uid), type(100))
+    
+    def testOneUser(self):
+        user = AccountModel.getOneUser(self.account.username)
+        self.assertEqual(self.account.uid, user.uid)
+        
+    def tearDown(self):
+        MySQLdatabase.ExecuteSafeInsertQuery("DELETE FROM User_ WHERE User_Name = %s", self.account.username)
+    
 
     
