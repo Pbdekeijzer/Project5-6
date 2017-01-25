31bc9aadc20b1cf6deabf0043ae3f65943ea8b57
FireStormHR <bret_hart_hitman_raw@hotmail.com>
2017-01-25 16:31:14 +0100

    only assert is wrong, call for help in previous commit is not needed

--- app/tests/test_requests.py
+++ app/tests/test_requests.py
@@ -13,10 +13,19 @@ class TestRequests(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_GetOneUser(self):
+    def test_GetOneUserOptionOne(self):
+        mock_account = AccountModel
+        mock_account.getOneUser = Mock(return_value=[("Name is not found")])
+        rv = self.app.get('/GetOneUser')
+        print(rv.data)
+        assert b'Username is not found' in rv.data
+
+    def test_GetOneUserOptionTwo(self):
         mock_account = AccountModel
         mock_account.getOneUser = Mock(return_value=[(87, 'Jopiejo', 'testje', 'test@test.test', '4564TR', 34, 1,1,1)])
-
+        rv = self.app.get('/GetOneUser')
+        print(rv.data)
+        assert b'Jopiejo' in rv.data
 
 
     def test_index(self):
@@ -31,7 +40,7 @@ class TestRequests(unittest.TestCase):
         mock_account.checkifExists = Mock(return_value=True)
 
         mock_history = HistoryModel
-        mock_history.get_oder_history = Mock(return_value="Something")
+        mock_history.get_order_history = Mock(return_value="Something")
         
         rv = self.app.get('/account/hoi/history')
         print(rv.data)
