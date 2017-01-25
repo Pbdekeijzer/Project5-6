0deb005998296ed1aa7e042ba8403318cb7bd00b
ThomOverhand <Thom_overhand@hotmail.com>
2017-01-25 12:20:33 +0100

    lol

--- app/tests/test_requests.py
+++ app/tests/test_requests.py
@@ -1,1 +1,20 @@
 import unittest
+from app import app
+from app.auth.authenticate import *
+
+
+class TestRequests(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_index(self):
+        rv = self.app.get('/')
+        assert b'Only in stock' in rv.data
+
+
+    
+
+    
