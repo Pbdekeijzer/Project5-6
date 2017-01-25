8805a408e704735fc06ae20c6b18c1df363fc67a
ThomOverhand <Thom_overhand@hotmail.com>
2017-01-25 14:29:25 +0100

    temp

--- app/tests/test_requests.py
+++ app/tests/test_requests.py
@@ -1,11 +1,14 @@
 import unittest
+from unittest import mock
+from unittest.mock import patch, MagicMock, Mock
 from app import app
-from app.auth.authenticate import *
-
+from app.models.AccountModel import *
+from app.models.HistoryModel import *
 
 class TestRequests(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
+        self.account = AccountModel(username="Henk", password="Test")
 
     def tearDown(self):
         pass
@@ -13,6 +16,20 @@ class TestRequests(unittest.TestCase):
     def test_index(self):
         rv = self.app.get('/')
         assert b'Only in stock' in rv.data
+
+
+    def test_purchase_history(self):
+        patch('app.requests.authenticate_user', lambda x: x).start()
+
+        mock_account = AccountModel
+        mock_account.checkifExists = Mock(return_value=True)
+
+        mock_history = HistoryModel
+        mock_history.get_oder_history = Mock(return_value="Something")
+        
+        rv = self.app.get('/account/hoi/history')
+        print(rv.data)
+        assert b'Something' in rv.data
 
 
     
