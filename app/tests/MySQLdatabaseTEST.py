65f0f77476a857bf69d805406a2be18807e6e471
FireStormHR <bret_hart_hitman_raw@hotmail.com>
2017-01-25 15:05:31 +0100

    I successfully wrote an unit test :)

--- app/tests/MySQLdatabaseTEST.py
+++ app/tests/MySQLdatabaseTEST.py
@@ -3,11 +3,20 @@ from unittest import mock
 from app.models.AccountModel import *
 
 class TestMySQLdatabase(unittest.TestCase):
+    def setUp(self):
+        Query = "INSERT INTO User_(Privacy_wishlist, Adminbool, User_Name, Wachtwoord, Email_address, Postal_code, House_number, Blockedbool) VALUES (True, False, %s, %s, %s, %s,%s, FALSE)"
+        username = 'Jopiejo'
+        password = 'imanunittest'
+        email = 'iman@unittest.com'
+        postalcode = '8956AT'
+        housenumber = 2
+        MySQLdatabase.ExecuteSafeInsertQuery(Query, username, password, email, postalcode, housenumber)
+
     def test_ExecuteSafeSelectQuery(self):
         Query = 'SELECT User_Name FROM User_ WHERE User_Name = %s ORDER BY User_Name'
-        arg = 'hoi'
+        arg = 'Jopiejo'
         result = MySQLdatabase.ExcecuteSafeSelectQuery(Query, arg)
-        self.assertEqual(result[0][0], 'hoi')
+        self.assertEqual(result[0][0], 'Jopiejo')
 
     def test_ExecuteSafeInsertQuery(self):
         Query = "INSERT INTO User_(Privacy_wishlist, Adminbool, User_Name, Wachtwoord, Email_address, Postal_code, House_number, Blockedbool) VALUES (True, False, %s, %s, %s, %s,%s, FALSE)"
@@ -20,4 +29,4 @@ class TestMySQLdatabase(unittest.TestCase):
         self.assertTrue(didthequerywork)
 
     def tearDown(self):
-        MySQLdatabase.ExecuteSafeInsertQuery("DELETE FROM User_ WHERE User_Name = 'Jopie'")
+        MySQLdatabase.ExecuteSafeInsertQuery("DELETE FROM User_ WHERE User_Name = 'Jopie' OR User_Name = 'Jopiejo'")
