8bc0c826261edf566e448ef6cc8e0fe39b81ccda
SG1995 <0898958@hr.nl>
2017-01-27 15:12:50 +0100

    test

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
-from app.posts import posts
+from app.posts import posts as p
 from app.requests import requests as r
 from app.auth import auth as a
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.secret_key = 'coconuts'
 CORS(app)
 
-app.register_blueprint(posts)
+app.register_blueprint(p)
 app.register_blueprint(r)
 app.register_blueprint(a)
 
